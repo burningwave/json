02f2a14a62e37851f42ecf9743eaf4f7e99dd207
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-10-23 21:18:54 +0200

    Added test

--- src/test/java/org/burningwave/json/ValidatorTest.java
+++ src/test/java/org/burningwave/json/ValidatorTest.java
@@ -1,6 +1,38 @@
 package org.burningwave.json;
 
-public class ValidatorTest extends BaseTest {
+import java.util.Collection;
 
+import org.burningwave.json.bean.Root;
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest extends BaseTest {
+
+	static final Facade facade = Facade.create();
+
+	@Test
+	void validateTestOne() {
+		testThrow(() -> {
+			facade.validator().registerCheck(
+				//Check
+				Check.forAll().checkMandatory(),
+				Check.forAllStringValues().execute(validationContext -> {
+
+				})
+			);
+
+			//Loading the JSON object
+			Root jsonObject = facade.objectMapper().readValue(
+				ObjectHandlerTest.class.getClassLoader().getResourceAsStream("quiz-to-be-validated.json"),
+				Root.class
+			);
+			ObjectHandler objectHandler = facade.newObjectHandler(jsonObject);
+			Collection<Throwable> exceptions = facade.validator().validate(Validation.Config.forJsonObject(jsonObject).withCompleteValidation());
+			for (Throwable exc : exceptions) {
+				System.err.println(exc.getMessage());
+			};
+			throw exceptions.iterator().next();
+		});
+
+	}
 
 }
